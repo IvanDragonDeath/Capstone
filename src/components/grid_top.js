c0cd3220ae369188376d847d794e358af2396f65
Kayden Forthman <forthmankayden@gmail.com>
2018-12-31 12:26:11 -0700

    Updated Search Engine function

--- src/components/grid_top.js
+++ src/components/grid_top.js
@@ -4,12 +4,12 @@ export function GridTop() {
     return (
         <div className="grid__top">
         <div className= "top__left">
-            <div className= "link_1"><a href="https://excitingnewsearchengine2.herokuapp.com/">Link_#</a></div>
-            <div className= "link_2"><a href="https://excitingnewsearchengine2.herokuapp.com/">Link_#</a></div>
+            <div className= "left_1"><a href="https://www.youtube.com/">Youtube</a></div>
+            <div className= "left_2"><a href="https://devcamp.com/users/sign_in">Devcamp</a></div>
         </div>
         <div className= "top__right">
-            <div className= "link_3"><a href="https://excitingnewsearchengine2.herokuapp.com/">Link_#</a></div>
-            <div className= "link_4"><a href="https://excitingnewsearchengine2.herokuapp.com/">Link_#</a></div>
+            <div className= "right_3"><a href="https://mail.google.com/mail/?authuser=0">Gmail</a></div>
+            <div className= "right_4"><a href="https://www.royalroad.com/home">RoyalRoad</a></div>
         </div>
     </div>
     )
